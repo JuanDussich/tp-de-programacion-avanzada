283c79525ba62b7d41daa71f31e2c92bebf9eeb0
JuanDussich <jmdussich@gmail.com>
2025-05-23 14:23:11 -0300

    Se agregaron validaciones

--- CentroDeSalud v2/src/repository/Validaciones.java
+++ CentroDeSalud v2/src/repository/Validaciones.java
@@ -1,5 +1,16 @@
 package repository;
 
 public interface Validaciones {
-
+	
+	
+	default String validarCaracteres(String Mensaje) {
+		
+		return Mensaje;
+	}
+	
+	default String validarNumeros(String Num) {
+		
+		return Num;
+	}
+	
 }
