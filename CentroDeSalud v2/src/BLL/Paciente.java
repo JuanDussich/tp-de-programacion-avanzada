184fefa370810e5cd7f175c0ec6ad5b873e8d2ad
JuanDussich <jmdussich@gmail.com>
2025-05-25 13:40:42 -0300

    se cambio todo a el controller de paciente

--- CentroDeSalud v2/src/BLL/Paciente.java
+++ CentroDeSalud v2/src/BLL/Paciente.java
@@ -22,7 +22,7 @@ public class Paciente extends Usuario implements Encriptador{
     private int id;
     private int dni;
     private String fechaNacimiento;
-    private static Connection con = Conexion.getInstance().getConnection();
+    
 //    private HistoriaClinica historiaClinica;
 
     // CONSTRUCTOR
@@ -36,108 +36,25 @@ public class Paciente extends Usuario implements Encriptador{
         this.fechaNacimiento = fechaNacimiento;
     }
     // METODOS
-
+    
     public static Paciente login(String email, String contrasenia) {
-        Paciente paciente = new Paciente();
-        try {
-            PreparedStatement stmt = con.prepareStatement(
-                    "SELECT * FROM paciente WHERE email = ? AND contrasenia = ?"
-            );
-            stmt.setString(1, email);
-            stmt.setString(2,paciente.encriptar(contrasenia));
-            //stmt.setString(2, contrasenia);
-            
-
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                int id = rs.getInt("idPaciente");
-                String nombre = rs.getString("nombre");
-                String apellido = rs.getString("apellido");
-                int dni = rs.getInt("dni");
-                String fechaNacimiento = rs.getString("fecha_De_nacimiento");
-
-                paciente =  new Paciente(id,nombre,apellido,dni, fechaNacimiento, email,contrasenia);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return paciente;
-    }
-
-    public static void agregarUsuario(Paciente usuario) {
-        try {
-            PreparedStatement statement = con.prepareStatement(
-                    "INSERT INTO `paciente`( `nombre`, `apellido`, `dni`, `fecha_De_Nacimiento`, `email`, `contrasenia`) VALUES (?,?,?,?,?,?)"
-            );
-            statement.setString(1, usuario.getNombre());
-            statement.setString(2, usuario.getApellido());
-            statement.setInt(3,usuario.getDni());
-            statement.setString(4, usuario.getFechaNacimiento());
-            statement.setString(5, usuario.getEmail());
-            statement.setString(6, usuario.encriptar(usuario.getContrasenia()));
-            //statement.setString(6, usuario.getContrasenia());
-
-            int filas = statement.executeUpdate();
-            if (filas > 0) {
-                System.out.println("Usuario agregado correctamente.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    	Paciente usuario = new Paciente() ;
+    	if (email.isEmpty() || contrasenia.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Hay un error, no puede ser vacio");
+		}else {
+			usuario = ControllerPaciente.login(email, contrasenia);
+			
+		}
+    	return usuario;
     }
 
     public static void RegistrarUsuario() {
-
-        String nombre = JOptionPane.showInputDialog("ingresa tu nombre");
-        String apellido = JOptionPane.showInputDialog("ingresa tu apellido");
-        int  dni = Integer.parseInt(JOptionPane.showInputDialog("ingresa tu dni"));
-        String fechaNacimiento = JOptionPane.showInputDialog("ingresa la fecha");
-
-        String email = JOptionPane.showInputDialog("ingresa tu email");
-        String contrasenia = JOptionPane.showInputDialog("ingresa tu contrasenia");
-        Paciente nuevo = new Paciente(0,nombre,apellido,dni,fechaNacimiento,email,contrasenia);
-
-        LinkedList<Paciente> existentes = mostrarPaciente();
-        boolean flag = true;
-        for (Usuario existente : existentes) {
-            if (existente.getEmail().equals(nuevo.getEmail())) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag) {
-            agregarUsuario(nuevo);
-        }else {
-            JOptionPane.showMessageDialog(null, "Usuario ya creado");
-        }
-
-
+    	
+    	ControllerPaciente.RegistrarPaciente();
+    	
+    	
     }
-    public static LinkedList<Paciente> mostrarPaciente() {
-        LinkedList<Paciente> usuarios = new LinkedList<>();
-        try {
-            PreparedStatement stmt = con.prepareStatement("SELECT * FROM paciente");
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                int id = rs.getInt("idPaciente");
-                String nombre = rs.getString("nombre");
-                String apellido = rs.getString("apellido");
-                int dni = rs.getInt("dni");
-                String fechaNacimiento = rs.getString("fecha_De_nacimiento");
-                String email = rs.getString("email");
-                String contrasenia = rs.getString("contrasenia");
-
-                usuarios.add(new Paciente(id, nombre,apellido,dni ,fechaNacimiento, email,contrasenia));
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return usuarios;
-    }
-
+    
     public void MenuPaciente() {
 
         int menu;
