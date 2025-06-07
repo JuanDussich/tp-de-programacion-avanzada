373393186c8e5c9699cafa5afce7b11cffe76379
Brailo01 <165711827+Brailo01@users.noreply.github.com>
2025-06-07 09:41:17 -0300

    Crud de ListaMedico

--- CentroDeSalud v2/src/BLL/ListaMedico.java
+++ CentroDeSalud v2/src/BLL/ListaMedico.java
@@ -2,34 +2,67 @@ package BLL;
 import java.util.ArrayList;
 
 public class ListaMedico {
-	
-	//ATRIBUTOS
-		private ArrayList<Medico> medicos;
-		// CONSTRUCTORES
-		public ListaMedico() {
-		        this.medicos = new ArrayList<>();
-		}
-		// METODOS
-		public void agregarMedico(Medico medico) {
-		        medicos.add(medico);
-		}
 
-		public void eliminarMedico(Medico medico) {
-		        medicos.remove(medico);
-		}
 
-		// GETTERS Y SETTERS
-		public ArrayList<Medico> getMedicos() {
-		        return medicos;
-		}
+    private ArrayList<Medico> medicos;
 
-		public void setMedicos(ArrayList<Medico> medicos) {
-		        this.medicos = medicos;
-		}
 
-		@Override
-		public String toString() {
-			return "ListaMedico{" + "medicos=" + medicos + '}';
-		}
+    public ListaMedico() {
+        this.medicos = new ArrayList<>();
+    }
 
+
+    public void agregarMedico(Medico medico) {
+        medicos.add(medico);
+    }
+
+
+    public Medico buscarPorMatricula(String matricula) {
+        for (Medico m : medicos) {
+            if (m.getMatricula().equalsIgnoreCase(matricula)) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public void mostrarTodos() {
+        for (Medico m : medicos) {
+            System.out.println(m);
+        }
+    }
+
+
+    public boolean actualizarMedico(String matricula, String nuevaEspecialidad, int nuevaCantidadConsultas) {
+        Medico medico = buscarPorMatricula(matricula);
+        if (medico != null) {
+            medico.setEspecialidad(nuevaEspecialidad);
+            medico.setCantidadConsultas(nuevaCantidadConsultas);
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean eliminarPorMatricula(String matricula) {
+        Medico medico = buscarPorMatricula(matricula);
+        if (medico != null) {
+            medicos.remove(medico);
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Medico> getMedicos() {
+        return medicos;
+    }
+
+    public void setMedicos(ArrayList<Medico> medicos) {
+        this.medicos = medicos;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaMedico{" + "medicos=" + medicos + '}';
+    }
 }
