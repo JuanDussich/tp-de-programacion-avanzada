91b6a6607c2ccc6c5b726f9483ff898ee5d7cd1f
JuanDussich <jmdussich@gmail.com>
2025-06-17 11:00:03 -0300

    upd corto en pantallas principales admin,medico,paciente

--- CentroDeSalud v2/src/GUI/PantallaAdministrador.java
+++ CentroDeSalud v2/src/GUI/PantallaAdministrador.java
@@ -4,6 +4,7 @@ import BLL.Administrador;
 import DLL.ControllerAdministrador;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -18,8 +19,22 @@ public class PantallaAdministrador extends JFrame {
     private JTable tablaAdministradores;
     private DefaultTableModel modeloTabla;
     private JButton btnEditar, btnEliminar, btnRefrescar;
+	private JPanel contentPane;
 
     public PantallaAdministrador() {
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 742, 551);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel LblTitulo = new JLabel("Centro De Salud");
+		LblTitulo.setForeground(Color.GRAY);
+		LblTitulo.setFont(new Font("Copperplate Gothic Light", Font.BOLD, 30));
+		LblTitulo.setBounds(225, 34, 293, 137);
+		contentPane.add(LblTitulo);
     	
     }
 }
