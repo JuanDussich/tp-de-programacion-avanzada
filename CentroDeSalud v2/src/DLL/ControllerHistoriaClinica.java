2cb9fc9611c86052291d9071d2d810f521c81263
Brailo01 <brailopez002@gmail.com>
2025-08-01 19:35:44 -0300

    upd
    HistoriaClinica:
    - Validación para mostrar solo historias clínicas del médico actual.
    - Validación de fecha (no permitir fechas futuras).
    - Validación de campos obligatorios.
    - DetalleConsulta.
    
    ControllerHistoriaClinica:
    - Validaciones.

--- CentroDeSalud v2/src/DLL/ControllerHistoriaClinica.java
+++ CentroDeSalud v2/src/DLL/ControllerHistoriaClinica.java
@@ -10,10 +10,10 @@ public class ControllerHistoriaClinica {
 
     private static Connection con = Conexion.getInstance().getConnection();
 
-    // CREATE
+
     public void crearHistoriaClinica(HistoriaClinica hc) {
-        String sql = "INSERT INTO historia_clinica (observaciones, fecha, turnoId, pacienteId, tratamientoId, medicamentoId, medicoId) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO historia_clinica (observaciones, fecha, turnoId, pacienteId, tratamientoId, medicamentoId, medicoId, detalleConsulta) " +
+                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         try {
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setString(1, hc.getObservaciones());
@@ -23,6 +23,7 @@ public class ControllerHistoriaClinica {
             stmt.setInt(5, hc.getTratamientoId());
             stmt.setInt(6, hc.getMedicamentoId());
             stmt.setInt(7, hc.getMedicoId());
+            stmt.setString(8, hc.getDetalleConsulta());
             stmt.executeUpdate();
 
             System.out.println("Historia clínica creada correctamente.");
@@ -31,7 +32,7 @@ public class ControllerHistoriaClinica {
         }
     }
 
-    // READ
+
     public ArrayList<HistoriaClinica> obtenerHistoriasClinicas() {
         ArrayList<HistoriaClinica> lista = new ArrayList<>();
         String sql = "SELECT * FROM historia_clinica";
@@ -48,7 +49,8 @@ public class ControllerHistoriaClinica {
                     rs.getInt("pacienteId"),
                     rs.getInt("tratamientoId"),
                     rs.getInt("medicamentoId"),
-                    rs.getInt("medicoId")
+                    rs.getInt("medicoId"),
+                    rs.getString("detalleConsulta")
                 );
                 lista.add(hc);
             }
@@ -58,7 +60,7 @@ public class ControllerHistoriaClinica {
         return lista;
     }
 
-    // UPDATE
+
     public void actualizarHistoriaClinica(int id, String nuevasObservaciones) {
         String sql = "UPDATE historia_clinica SET observaciones = ? WHERE idHistorialMedico = ?";
         try {
@@ -72,7 +74,7 @@ public class ControllerHistoriaClinica {
         }
     }
 
-    // DELETE
+
     public void eliminarHistoriaClinica(int id) {
         String sql = "DELETE FROM historia_clinica WHERE idHistorialMedico = ?";
         try {
@@ -85,3 +87,5 @@ public class ControllerHistoriaClinica {
         }
     }
 }
+
+
